f290f9268482e781d0c71358e80c30cb9c6fd4f8
Mark <mark.aaron.fisher@gmail.com>
2017-02-17 15:22:45 -0800

    add CDN for jQuery script and update websiteRepoSetupIndivid.sh to reflect changes in websiteRepoSetup.sh

--- websiteRepoSetupIndivid.sh
+++ websiteRepoSetupIndivid.sh
@@ -3,12 +3,15 @@
 mkdir ~/Desktop/$1
 mkdir ~/Desktop/$1/css
 mkdir ~/Desktop/$1/img
+mkdir ~/Desktop/$1/js
 touch ~/Desktop/$1/css/styles.css
 touch ~/Desktop/$1/index.html
+touch ~/Desktop/$1/js/scripts.js
+cp ./index.html ~/Desktop/$1/
 cd ~/Desktop/$1
 git init
 git config --global user.name $2
 git config --global user.email $3
 git add .
-git commit -m "create initial css folder, img folder, and index.html"
+git commit -m "create initial css folder with styles.css, js folder with scripts.js, img folder, and a template index.html"
 atom .
